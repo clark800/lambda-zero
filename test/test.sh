17ad6c63ab8c9397a62b78675192d7e0dc15a44c
Chris Clark <clark800@gmail.com>
2018-02-01 16:37:23 -0800

    Fix status code of test.sh

--- test/test.sh
+++ test/test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -euo pipefail
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
@@ -9,7 +10,7 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 CMD="$DIR/../main -t"
 
 function header {
-    echo -e "${BLUE}==== $1 ====${NOCOLOR}"
+    echo -e "${BLUE}========== $1 ==========${NOCOLOR}"
 }
 
 function check {
@@ -19,6 +20,7 @@ function check {
     if [[ "$output" == "$expected_output" ]]; then
         echo -en "${GREEN}PASS${NOCOLOR} "
         echo "$label"
+        return 0
     else
         echo -en "${RED}FAIL${NOCOLOR} "
         echo "$label"
@@ -26,39 +28,56 @@ function check {
         echo "$expected_output"
         echo "=== OUTPUT ==="
         echo "$output"
+        return 1
     fi
 }
 
 function oneline_suite {
-    local name="$1"
-    local testcases_path="$DIR/$2"
-    [ ! -z "$3" ] && local prelude="$(cat "$DIR/$3")\n"
+    local testcases_path="$DIR/$1"
+    local name="$(basename "$testcases_path" ".test")"
+    local failures=0
+    local prelude=""
+    if [[ "$#" -eq 2 ]]; then
+        prelude="$(cat "$DIR/$2")\n"
+    fi
     header "$name"
-    grep -v "====" "$testcases_path" | while read -r line; do
+    while read -r line; do
         read expected_output
         local input="$prelude$line"
         local output=$(echo -e "$input" | $CMD 2>&1)
-        check "$line" "$expected_output" "$output"
-    done
+        check "$line" "$expected_output" "$output" || ((failures++)) || true
+    done < <(grep -v "====" "$testcases_path")
+    if [[ $failures -eq 0 ]]; then return 0; else return 1; fi
 }
 
-function suite {
-    local name="$1"
-    local fixtures="$2"
-    local regexp="${3:-.*}"
-
-    header "$name"
-    for filepath in $DIR/$fixtures/*.in; do
-        local label=$(basename $filepath .in)
-        if [[ $label =~ $regexp ]]; then
-            local expected_output=$(cat $DIR/$fixtures/$label.out)
-            local output=$(cd "$DIR/.." && cat $filepath | $CMD 2>&1)
-            check "$label" "$expected_output" "$output"
-        fi
-    done
+function summarize {
+    local failures="$1"
+    header "summary"
+    if [[ "$failures" -eq 0 ]]; then
+        echo -en "${GREEN}PASS${NOCOLOR} "
+        echo "all tests passed"
+        exit 0
+    else
+        echo -en "${RED}FAIL${NOCOLOR} "
+        echo "$failures test suite(s) failed"
+        exit 1
+    fi
 }
 
-oneline_suite "lambda" "lambda.test" ""
-oneline_suite "arithmetic" "arithmetic.test" ""
-oneline_suite "definition" "definition.test" ""
-oneline_suite "prelude" "prelude.test" "prelude"
+function run {
+    local failures=0
+    declare -a suites=(
+        "lambda.test"
+        "arithmetic.test"
+        "definition.test"
+        "prelude.test prelude"
+    )
+    for suite in "${suites[@]}"; do
+        # the next line allows us to continue running tests after a
+        # suite fails with set -e mode enabled
+        oneline_suite $suite || ((failures++)) || true
+    done
+    summarize "$failures"
+}
+
+run
