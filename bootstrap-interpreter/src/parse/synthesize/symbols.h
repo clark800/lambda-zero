2102f849490488d2188becc818c7ba16d8b7e734
Chris Clark <clark800@gmail.com>
2018-12-25 11:52:50 -0800

    Affix syntax declarations

--- bootstrap-interpreter/src/parse/synthesize/symbols.h
+++ bootstrap-interpreter/src/parse/synthesize/symbols.h
@@ -5,7 +5,10 @@ typedef unsigned char Precedence;
 bool isSpecial(Node* node);
 bool isOpenOperator(Node* node);
 void erase(Stack* stack, const char* lexeme);
+Associativity getAssociativity(Node* operator);
 Fixity getFixity(Node* operator);
+String getPrior(Node* operator);
+Precedence findPrecedence(Node* node);
 
 Node* reduce(Node* operator, Node* left, Node* right);
 Node* reduceBracket(Node* open, Node* close, Node* left, Node* right);
@@ -16,9 +19,8 @@ Node* parseSymbol(Tag tag, long long value);
 void addCoreSyntax(const char* symbol, Precedence leftPrecedence,
     Precedence rightPrecedence, Fixity fixity, Associativity associativity,
     void (*shift)(Stack*, Node*), Node* (*reduce)(Node*, Node*, Node*));
-void addSyntax(Tag tag, Precedence leftPrecedence, Precedence rightPrecedence,
-    Fixity fixity, Associativity associativity, void (*shifter)(Stack*, Node*),
-    Node* (*reducer)(Node*, Node*, Node*));
-void addMixfixSyntax(Tag tag, Node* prior, void (*shifter)(Stack*, Node*));
+void addSyntax(Tag tag, String prior, Precedence leftPrecedence,
+    Precedence rightPrecedence, Fixity fixity, Associativity associativity,
+    void (*shifter)(Stack*, Node*), Node* (*reducer)(Node*, Node*, Node*));
 void addSyntaxCopy(String lexeme, Node* name, bool alias);
 void addCoreAlias(const char* alias, const char* name);
